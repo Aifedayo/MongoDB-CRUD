4a47b615075b32d70a4780e6f9348602e98fade4
Aifedayo <akeemifedayolag@gmail.com>
2024-03-02 10:00:00 +0100

    schema validation

---
+++ main2.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+import pprint
+
+from pymongo import MongoClient
+
+load_dotenv(find_dotenv())
+
+password = os.environ.get("MONGODB_PWD")
+
+connection_string = f"mongodb+srv://akeemifedayolag:{password}@tutorial-cluster.k5y6y.mongodb.net/?retryWrites=true&w=majority&appName=Tutorial-Cluster"
+
+client = MongoClient(connection_string)
+dbs = client.list_database_names()
+production = (client.production)
+
+# SCHEME VALIDATION
